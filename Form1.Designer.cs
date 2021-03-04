819b537ddb449eb58feb40f424b24a46b8de58ee
Kittia6 <emeraldkittia6@gmail.com>
2021-03-03 22:40:01 -0500

    almost done?

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnInt = new System.Windows.Forms.Button();
+            this.btnDouble = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(191, 104);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(239, 22);
+            this.txtMax.TabIndex = 0;
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(191, 191);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(239, 22);
+            this.txtMin.TabIndex = 1;
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("SimSun-ExtB", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(101, 21);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(317, 19);
+            this.lblInstructions.TabIndex = 2;
+            this.lblInstructions.Text = "Enter a maximum and minimum:";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Font = new System.Drawing.Font("SimSun-ExtB", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMax.Location = new System.Drawing.Point(66, 104);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(97, 19);
+            this.lblMax.TabIndex = 3;
+            this.lblMax.Text = "Maximum:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("SimSun-ExtB", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(66, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Minimum:";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("SimSun-ExtB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(103, 331);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(88, 24);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "Result";
+            // 
+            // btnInt
+            // 
+            this.btnInt.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnInt.Font = new System.Drawing.Font("SimSun-ExtB", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInt.Location = new System.Drawing.Point(70, 254);
+            this.btnInt.Name = "btnInt";
+            this.btnInt.Size = new System.Drawing.Size(170, 38);
+            this.btnInt.TabIndex = 6;
+            this.btnInt.Text = "Get Integer";
+            this.btnInt.UseVisualStyleBackColor = false;
+            this.btnInt.Click += new System.EventHandler(this.btnInt_Click);
+            // 
+            // btnDouble
+            // 
+            this.btnDouble.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDouble.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnDouble.Font = new System.Drawing.Font("SimSun-ExtB", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDouble.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnDouble.Location = new System.Drawing.Point(272, 254);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(170, 38);
+            this.btnDouble.TabIndex = 7;
+            this.btnDouble.Text = "Get Double";
+            this.btnDouble.UseVisualStyleBackColor = false;
+            this.btnDouble.Click += new System.EventHandler(this.btnDouble_Click);
             // 
             // RandomNumbers
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(506, 394);
+            this.Controls.Add(this.btnDouble);
+            this.Controls.Add(this.btnInt);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.txtMax);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "RandomNumbers";
             this.Text = "Random Numbers";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnInt;
+        private System.Windows.Forms.Button btnDouble;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,9 +13,27 @@ namespace Part_4___Random_Assignment
     public partial class RandomNumbers : Form
     {
         Random generator = new Random();
+        int maxInt,minInt;
+        
+
         public RandomNumbers()
         {
             InitializeComponent();
+        }
+
+        private void btnInt_Click(object sender, EventArgs e)
+        {
+            maxInt = Convert.ToInt32(txtMax.Text);
+            minInt = Convert.ToInt32(txtMin.Text);
+            generator.Next(minInt, maxInt);
+            
+        }
+
+        private void btnDouble_Click(object sender, EventArgs e)
+        {
+            maxInt = Convert.ToInt32(txtMax.Text);
+            minInt = Convert.ToInt32(txtMin.Text);
+           Convert.ToDouble(generator.Next(minInt, maxInt));
         }
     }
 }
